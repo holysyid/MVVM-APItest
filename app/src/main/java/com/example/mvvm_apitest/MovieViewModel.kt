7bc67cd470c6a9b4cf7fa18f791dab2ab4131411
holysyid <holysyid@gmail.com>
2022-12-28 03:36:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mvvm_apitest/MovieViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.mvvm_apitest
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MovieViewModel : ViewModel() {
+    private var movieLiveData = MutableLiveData<List<Result>>()
+    fun getPopularMovies() {
+        RetrofitInstance.api.getPopularMovies("69d66957eebff9666ea46bd464773cf0").enqueue(object :
+            Callback<Movie> {
+            override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
+                if (response.body()!=null){
+                    movieLiveData.value = response.body()!!.results
+                }
+                else{
+                    return
+                }
+            }
+            override fun onFailure(call: Call<Movie>, t: Throwable) {
+                Log.d("TAG",t.message.toString())
+            }
+        })
+    }
+    fun observeMovieLiveData() : LiveData<List<Result>> {
+        return movieLiveData
+    }
+}
